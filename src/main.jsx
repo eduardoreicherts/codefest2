38db4988072366845b4bec36f1af23bda78d00ed
eduardoreicherts <dudureichert86@gmail.com>
2023-12-09 11:04:08 -0300

    .

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Bicentenario from './pages/bicentenario/index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Bicentenario/>
+  </React.StrictMode>,
+)
---
+++ src/pages/bicentenario/index.jsx
@@ -0,0 +1,20 @@
+import SwipeableTextMobileStepper from "../../components/Header";
+import "./index.css";
+
+
+
+const Bicentenario = () => {
+    return (
+        <>
+        <SwipeableTextMobileStepper/>
+        <section className="sec1">
+            1
+        </section>
+        <section className="sec2">
+            2
+        </section>
+        </>
+    );
+}
+
+export default Bicentenario
